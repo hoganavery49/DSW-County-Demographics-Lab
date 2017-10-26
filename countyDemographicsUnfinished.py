37fc2600341770ee678c35cb13fa025472ff0696
Avery Hogan <hogan.avery49@gmail.com>
2017-10-26 10:13:07 -0700

    Update countyDemographicsUnfinished.py

--- countyDemographicsUnfinished.py
+++ countyDemographicsUnfinished.py
@@ -7,23 +7,43 @@ def main():
     print(county_most_under_18(counties))
     print(percent_most_under_18(counties))
     print(most_under_18(counties))
-    print(state_with_most_counties(counties))
+    #print(state_with_most_counties(counties))
 
 def alphabetically_first_county(counties):
     """Return the county with the name that comes first alphabetically."""
-
+    first = counties[0]["County"]
+    for county in counties:
+        if county["County"] < first:
+            first = county["County"]
+    
+    return first
 
 def county_most_under_18(counties):
     """Return the name and state of a county ("<county name>, <state>") with the highest percent of under 18 year olds."""
-
+    highestPercentage = counties[0]["Age"]["Percent Under 18 Years"]
+    countyID = [counties[0]["County"], counties[0]["State"]]
+    for county in counties:
+        if county["Age"]["Percent Under 18 Years"] > highestPercentage:
+            highestPercentage = county["Age"]["Percent Under 18 Years"]
+            countyID = [county["County"], county["State"]]
+            
+    return countyID
     
 def percent_most_under_18(counties):
     """Return the highest percent of under 18 year olds."""
-
+    highestPercentage = counties[0]["Age"]["Percent Under 18 Years"]
+    for county in counties:
+        if county["Age"]["Percent Under 18 Years"] > highestPercentage:
+            highestPercentage = county["Age"]["Percent Under 18 Years"]
+            
+    return highestPercentage
     
 def most_under_18(counties):
     """Return a list with the name and state of a county ("<county name>, <state>") and the percent of under 18 year olds for a county with the highest percent of under 18 year olds."""
-
+    county = county_most_under_18(counties)
+    percentage = percent_most_under_18(counties)
+    
+    return [county, percentage]
     
 def state_with_most_counties(counties):
     """Return a state that has the most counties."""
